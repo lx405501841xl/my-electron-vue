dc59c74ca06478d751fefe97b24b1c9b98884d05
yanji_ <w2c17lin@qq.com>
2017-08-29 17:55:14 +0800

    init

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <Row>
+        <Form ref="formCustom" :model="formCustom" :rules="ruleCustom" :label-width="80">
+          <FormItem prop="text">
+            <Input placeholder="请输入账号" type="text"></Input>
+          </FormItem>
+          <FormItem prop="passwd">
+            <Input placeholder="请输入密码" type="password"></Input>
+          </FormItem>
+          <FormItem>
+            <Button type="primary" @click="handleSubmit('formCustom')">登陆</Button>
+          </FormItem>
+        </Form>
+      </Row>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landing-page',
+  methods: {
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          this.$Message.success('提交成功!')
+        } else {
+          this.$Message.error('表单验证失败!')
+        }
+      })
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
+    }
+  }
+}
+</script>
+
+<style>
+Row {
+  text-align: center;
+}
+
+Form {
+  margin-top: 80px;
+}
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css' // 使用 CSS
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.use(iView)
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: {
+    App
+  },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
