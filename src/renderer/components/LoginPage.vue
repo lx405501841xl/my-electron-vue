d0e683c55cdbacd35595e9367f5a58bc711396b4
yanji_ <w2c17lin@qq.com>
2017-09-01 08:20:29 +0800

    add vue

---
+++ src/renderer/components/LoginPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <el-row>
+        <el-form ref="form">
+          <el-form-item prop="text">
+            <el-input v-model="account" placeholder="请输入账号" type="text"></el-input>
+          </el-form-item>
+          <el-form-item prop="passwd">
+            <el-input v-model="password" placeholder="请输入密码" type="password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="handleSubmit()">登陆</el-button>
+          </el-form-item>
+        </el-form>
+      </el-row>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login-page',
+  data () {
+    return {
+      account: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleSubmit () {
+      if (this.account === 'admin' && this.password === 'qwe123') {
+        this.$router.push('/main') // 路由到main
+      } else {
+        this.$message.error('账号或密码错误!')
+      }
+    },
+    handleReset () {
+      this.account = ''
+      this.password = ''
+    }
+  }
+}
+</script>
+
+<style>
+.el-form {
+  margin-top: 80px;
+  width: 280px;
+  text-align: center;
+  margin: 80px auto;
+}
+</style>
---
+++ src/renderer/components/MainPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-row>
+    <div class="total-msg" v-bind:style="{height: css.totalHeight + 'px'}"></div>
+    <div class="self-msg">
+      <el-input type="textarea" resize="none" :autosize="{ minRows: 4, maxRows: 4}" v-model="msg">
+      </el-input>
+      <el-button type="primary">发送</el-button>
+    </div>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'main-page',
+  data () {
+    return {
+      msg: '',
+      all: []
+    }
+  },
+  computed: {
+    css: function () {
+      return {
+        totalHeight: document.body.scrollHeight - 168
+      }
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+    this.$notify({
+      title: '提示',
+      type: 'success',
+      message: '现在可以进行聊天了哦!',
+      duration: 0
+    })
+  }
+}
+</script>
+
+<style>
+.total-msg {
+  border: 1px solid #bfcbd9;
+  background-color: white;
+}
+
+.self-msg {
+  margin-top: 8px;
+  border: 1px solid #bfcbd9;
+  text-align: right;
+  background-color: white;
+}
+
+.self-msg .el-textarea__inner {
+  border: none;
+}
+
+.self-msg .el-button {
+  margin: 5px;
+}
+</style>
